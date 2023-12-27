8dafb6f64f8faf1fc56b2876d752e36904b299f8
gsheir <gsheir@outlook.com>
2023-12-27 14:16:29 +0000

    add wsl pass viz

--- src/utils/statsbomb_wrappers.py
+++ src/utils/statsbomb_wrappers.py
@@ -6,9 +6,20 @@ from enum import Enum
 
 def ignore_warnings():
     warnings.simplefilter("ignore")
-log = logging.getLogger()
 
+ignore_warnings()
+log = logging.getLogger()
 logging.basicConfig(level=logging.INFO)
+
+def search_in_dataframe(dataframe, **kwargs):
+    if len(kwargs) == 0:
+        return dataframe
+    
+    condition = True
+    for column_name, value in kwargs.items():
+        condition = condition & (dataframe[column_name] == value)
+        
+    return dataframe.loc[condition]
 
 class Competitions:
     def __init__(self) -> None:
@@ -17,15 +28,44 @@ class Competitions:
     def get_season(self, competition_name, season_name):
         condition = (self.competitions["competition_name"] == competition_name) & (self.competitions["season_name"] == season_name)
         season_row = self.competitions.loc[condition]
+
+        assert len(season_row) == 1, f"Failed to get season, Statsbomb API should return 1 season, not {len(season_row)}"
+        
         try:
             season = Season(
-            competition_id=season_row["competition_id"].values[0], 
-            season_id=season_row["season_id"].values[0]
+            competition_id=season_row["competition_id"].iloc[0], 
+            season_id=season_row["season_id"].iloc[0]
         )
+            season.load_competition_events(season_row["country_name"].iloc[0], season_row["competition_name"].iloc[0], season_row["season_name"].iloc[0], season_row["competition_gender"].iloc[0])
             
         except Exception as e:
             raise ValueError("Failed to get season, Statsbomb API returned error", e) 
         return season
+    
+    
+
+class Season:
+    def __init__(self, competition_id, season_id) -> None:
+        self.matches = sb.matches(competition_id, season_id)
+        self.events = None
+
+    def load_competition_events(self, country, competition_name, season_name, gender):
+        self.events = sb.competition_events(
+            country=country,
+            division=competition_name,
+            season=season_name,
+            gender=gender
+        )   
+         
+    def get_match_by_id(self, match_id):
+        return self.matches.loc[match_id]
+    
+    def get_matches(self, **kwargs):
+        return search_in_dataframe(self.matches, **kwargs)
+    
+    def get_events(self, **kwargs):
+        return search_in_dataframe(self.events, **kwargs)
+
 
 class Match:
     def __init__(self, match_id) -> None:
@@ -38,29 +78,8 @@ class Match:
         return self.events.loc[event_id]
     
     def get_events(self, **kwargs):
-        condition = True;
-        for column_name, value in kwargs.items():
-            condition = condition & (self.events[column_name] == value)
-        
-        return self.events.loc[condition]
-        
-        
-class Season:
-    def __init__(self, competition_id, season_id) -> None:
-        self.matches = sb.matches(competition_id, season_id)
-
-    def get_match_by_id(self, match_id):
-        return self.matches.loc[match_id]
-    
-    def get_matches(self, **kwargs):
-        if len(kwargs) == 0:
-            return self.matches
-        
-        condition = True
-        for column_name, value in kwargs.items():
-            condition = condition & (self.matches[column_name] == value)
-            
-        return self.matches.loc[condition]
+        return search_in_dataframe(self.events, **kwargs)
+     
 
 class PitchDim(Enum):
     WIDTH = 80
