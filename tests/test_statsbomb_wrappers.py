4ef2539fed0333a8d63a3313956324c901dcd48a
gsheir <gsheir@outlook.com>
2024-10-18 10:59:02 +0100

    old changes

---
+++ tests/test_statsbomb_wrappers.py
@@ -0,0 +1,8 @@
+import pytest
+from utils.statsbomb_wrappers import PitchDim
+
+def test_pitch_dims():
+    assert PitchDim.DEF_GOAL_LPOST.value == (0, 36)
+    assert PitchDim.DEF_GOAL_RPOST == (0, 44)
+    assert PitchDim.ATT_GOAL_LPOST == (120, 36)
+    assert PitchDim.ATT_GOAL_RPOST == (120, 44)
